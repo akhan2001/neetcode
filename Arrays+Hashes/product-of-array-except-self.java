441a7659518500c6220a94d3cde89b2bf75823c9
akhan2001 <m.abdullah.khan567@gmail.com>
2024-12-31 22:56:58 -0500

    Product of Array Except Self

--- Arrays+Hashes/product-of-array-except-self.java
+++ Arrays+Hashes/product-of-array-except-self.java
@@ -1,17 +1,25 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int[] answer = new int[nums.length];
+        int[] prefix = new int[nums.length];
+        int[] postfix = new int[nums.length];
+    
+        prefix[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            prefix[i] = prefix[i - 1] * nums[i - 1];
+        }
+
+        postfix[nums.length - 1] = 1;
+        for (int i = nums.length - 2; i >= 0 ; i--) {
+            postfix[i] = postfix[i + 1] * nums[i + 1];
+        }
+
         for (int i = 0; i < nums.length; i++) {
-            int product = 1;
-            for (int j = 0; j < nums.length; j++) {
-                if (j != i) {
-                    // System.out.println(j);
-                    product = product * nums[j];
-                    System.out.println(nums[j]);
-                    // System.out.println(product);
-                    answer[i] = product;
-                }
-            }
+            answer[i] = prefix[i] * postfix[i];
+        }
+         
+        for (int n : answer) {
+            System.out.println(n);
         }
 
         return answer;
