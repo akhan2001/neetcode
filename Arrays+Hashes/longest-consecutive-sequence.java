e06f6415f2509b5f1b22ce4341c4e8e6212d7d70
akhan2001 <m.abdullah.khan567@gmail.com>
2025-01-04 18:54:47 -0500

    Consecutive Solution: O(n^2)

--- Arrays+Hashes/longest-consecutive-sequence.java
+++ Arrays+Hashes/longest-consecutive-sequence.java
@@ -1,14 +1,37 @@
+import java.util.HashSet;
+import java.util.Arrays;
+import java.util.Set;
+
 class Solution {
     public int longestConsecutive(int[] nums) {
+        // Arrays.sort(nums);
+        if (nums.length == 0) { return 0; }
+
         Set<Integer> consecutiveHash = new HashSet<>();
         int count = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            consecutiveHash.add(nums[i]);
+        for (int n : nums) {
+            consecutiveHash.add(n);
         }
 
-        System.out.println(consecutiveHash);
+        int longestStreak = 0;
 
-        return count;
+        for (int num : consecutiveHash) {
+            if (!consecutiveHash.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (consecutiveHash.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentStreak++;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        System.out.println(longestStreak);
+
+        return longestStreak;
     }
 }
