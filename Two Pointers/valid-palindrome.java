df699587e961d86ac14dc953f7e4566b9d138745
akhan2001 <m.abdullah.khan567@gmail.com>
2025-01-06 12:40:12 -0500

    Passed Test Cases

--- Two Pointers/valid-palindrome.java
+++ Two Pointers/valid-palindrome.java
@@ -1,8 +1,29 @@
 class Solution {
     public boolean isPalindrome(String s) {
         // Converting all uppercase letters to lowercase. Removing all non-alphanumeric characters
-        String str = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        System.out.println(str);
+        String str = s.replaceAll("[^a-zA-Z]", "").toLowerCase();
+        // System.out.println(str);
+
+        char c1 = ' ';
+        char c2 = ' ';
+        int i = 0;
+        int j = str.length() - 1;
+
+        while (i < str.length() / 2) {            
+            c1 = str.charAt(i);
+            c2 = str.charAt(j);
+            // System.out.println("i=" + i + " j=" + j);
+
+            // System.out.println(c1 + " - " + c2);
+
+            if (c1 != c2) {
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
         return true;
     }
 }
