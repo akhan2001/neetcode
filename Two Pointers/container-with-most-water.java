21ee3b01b5d5f6df3bcdfbe196b8784f629cc7b7
akhan2001 <m.abdullah.khan567@gmail.com>
2025-01-13 11:45:50 -0500

    printing out

--- Two Pointers/container-with-most-water.java
+++ Two Pointers/container-with-most-water.java
@@ -2,28 +2,10 @@ class Solution {
     public int maxArea(int[] height) {
         int maxAmount = 0;
 
-        // for (int h : height) {
-        //     System.out.println(h);
-        // }
-
-        int i = 0;
-        int j = height.length - 1;
-        while (i < j) {
-            int amount = (j - i) * Math.min(height[i], height[j]);
-            // System.out.println("Min height: " + amount);
-            if (height[i] < height[j]) {
-                i++;
-            } else if (height[i] > height[j]) {
-                j--;
-            } else {
-                i++;
-                j--;
-            }
-            
-            if (amount > maxAmount) {
-                maxAmount = amount;
-            }
+        for (int h : height) {
+            System.out.println(h);
         }
+
 
         return maxAmount;
     }
