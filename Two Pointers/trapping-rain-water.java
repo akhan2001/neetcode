ab3641a6867aeda66a3749d687695d0f08ef74b2
akhan2001 <m.abdullah.khan567@gmail.com>
2025-01-15 11:15:42 -0500

    comments

--- Two Pointers/trapping-rain-water.java
+++ Two Pointers/trapping-rain-water.java
@@ -1,5 +1,32 @@
 class Solution {
     public int trap(int[] height) {
-        
+        if (height.length < 3) return 0;
+
+        int i = 0, j = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        int water = 0;
+
+        while (i < j) {
+            if (height[i] < height[j]) {
+                if (height[i] > leftMax) {
+                    leftMax = height[i];
+                    System.out.println("leftMax = " + leftMax);
+                } else {
+                    water += leftMax - height[i];
+                    System.out.println("Water (left): " + water);
+                }
+                i++;
+            } else {
+                if (height[j] > rightMax) {
+                    rightMax = height[j];
+                    System.out.println("RightMax = " + rightMax);
+                } else {
+                    water += rightMax - height[j];
+                    System.out.println("Water (right): " + water);
+                }
+                j--;
+            }
+        }
+        return water;
     }
 }
