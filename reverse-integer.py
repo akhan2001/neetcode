8d2cda1456996c3af06096b780650f188f97ca5b
akhan2001 <m.abdullah.khan567@gmail.com>
2025-01-27 10:00:12 -0500

    32bit ints only

--- reverse-integer.py
+++ reverse-integer.py
@@ -11,3 +11,7 @@ class Solution(object):
             result = result * 10 + x % 10
             x = x // 10
         
+        if result > 2**31 - 1:
+            return 0
+        else:
+            return result
