6982f249fd7c6ded7e9d845297bceef5b0e579c3
akhan2001 <m.abdullah.khan567@gmail.com>
2025-01-27 09:59:21 -0500

    reversing integer

---
+++ reverse-integer.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x < 0:
+            return self.reverse(-x)
+        result = 0
+        while x > 0:
+            result = result * 10 + x % 10
+            x = x // 10
+        
