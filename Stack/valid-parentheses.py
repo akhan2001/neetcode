a588e54665f4e881d57cb3ee77d5dbda41aa6d73
akhan2001 <m.abdullah.khan567@gmail.com>
2025-02-09 13:45:09 -0500

    stack approach

--- Stack/valid-parentheses.py
+++ Stack/valid-parentheses.py
@@ -14,7 +14,16 @@ class Solution(object):
         stack = []
         for char in s:
             if char in ["(", "[", "{"]:
+
                 stack.append(char)
             elif char in [")", "]", "}"]:
                 if not stack:
                     return False
+                top = stack.pop()
+                if char == ")" and top != "(":
+                    return False
+                if char == "]" and top != "[":
+                    return False
+                if char == "}" and top != "{":
+                    return False
+        return not stack
